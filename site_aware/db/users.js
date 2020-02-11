8081b050e1e59490fc6d9b147062929d7e01e972
mayabridgewater <55682563+mayabridgewater@users.noreply.github.com>
2020-02-11 15:10:57 +0200

    new user

---
+++ site_aware/db/users.js
@@ -0,0 +1,43 @@
+const {connection} = require('./config');
+
+function checkEmail(email) {
+    return new Promise((resolve, reject) => {
+        connection.query(`select * from user where email = ?`, [email], function(error, results, fields) {
+            if(error) {
+                reject(error)
+            }else {
+                resolve(results)
+            }
+        })
+    })
+};
+
+function newRegister({first_name, last_name, email, register}) {
+    return new Promise((resolve, reject) => {
+        connection.query(`call add_user(?,?,?,?)`, [first_name, last_name, email, register], function(error, results, fields) {
+            if(error) {
+                reject(error)
+            }else {
+                resolve(results)
+            }
+        })
+    })
+};
+
+function newDetails(id, {address, city, phone}) {
+    return new Promise((resolve, reject) => {
+        connection.query(`call add_details(?,?,?,?)`, [id, address, city, phone], function(error, results, fields) {
+            if(error) {
+                reject(error)
+            }else {
+                resolve(results)
+            }
+        })
+    })
+}
+
+module.exports = {
+    checkEmail,
+    newRegister,
+    newDetails
+}
